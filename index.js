89aa5ec795f34f5fa4c5def6f07d7fe934c79285
Juan-David-Orozco <ing.ele2018@gmail.com>
2022-09-30 15:11:05 -0500

    project setup initial

---
+++ index.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJB3HLqc1fpqQ0zqYQBWwSj-kfxrgFy_Y",
+  authDomain: "crud-fb9.firebaseapp.com",
+  projectId: "crud-fb9",
+  storageBucket: "crud-fb9.appspot.com",
+  messagingSenderId: "580956242851",
+  appId: "1:580956242851:web:f49fe9e908783243fb5016"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
